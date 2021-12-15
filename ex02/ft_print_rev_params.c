893656570a5c92487277a5b3b57ba4ad45f52df6
Shade St Jude <shade.st.jude84@gmail.com>
2021-12-15 17:13:16 +1030

    ok

--- ex02/ft_print_rev_params.c
+++ ex02/ft_print_rev_params.c
@@ -1,10 +1,25 @@
 #include <unistd.h>
 
-//print reverse parameters
-
-int	main(int argc, char **argv)
+void	ft_putchar(char c)
 {
-    
+	write(1, &c, 1);
 }
 
-// argv[n] last argument
+void	ft_putstr(char *str)
+{
+	while (*str)
+	{
+		write(1, str, 1);
+		str++;
+	}
+}
+
+int	main(int argc, char *argv[])
+{
+	while (argc > 1)
+	{
+		ft_putstr(argv[argc - 1]);
+		ft_putchar('\n');
+		argc--;
+	}
+}
