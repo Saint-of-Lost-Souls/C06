384e40134f04563b960fae9078b0dd255862411a
Shade St Jude <shade.st.jude84@gmail.com>
2021-12-15 16:26:37 +1030

    update

---
+++ ex02/ft_print_rev_params.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+
+//print reverse parameters
+
+int	main(int argc, char **argv)
+{
+    int i;
+     
+    i = 1;
+    while (i < argc)
+	{
+		while (*argv[i])
+		{
+			write(1, argv[i], 1);
+			argv[i]++;
+		}
+		write(1, "\n", 1);
+		i++;
+	}
+}
