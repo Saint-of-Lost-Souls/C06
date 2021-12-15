0b50d5f2c17032d90a95c1352b5b57bc4bc82217
Shade St Jude <shade.st.jude84@gmail.com>
2021-12-15 16:55:46 +1030

    update

--- ex02/ft_print_rev_params.c
+++ ex02/ft_print_rev_params.c
@@ -4,17 +4,7 @@
 
 int	main(int argc, char **argv)
 {
-    int i;
-     
-    i = 1;
-    while (i < argc)
-	{
-		while (*argv[i])
-		{
-			write(1, argv[i], 1);
-			argv[i]++;
-		}
-		write(1, "\n", 1);
-		i++;
-	}
+    
 }
+
+// argv[n] last argument
